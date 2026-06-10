2c1e5a7b8d5247e8d1eb18037786ae7f76a754d5
amankumar-git9868 <amandhananjay75@gmail.com>
2026-06-10 13:29:32 +0530

    practicisng Python

---
+++ debugging.py
@@ -0,0 +1,16 @@
+butterflies = 10
+beetles = 12
+ladybugs = 20
+v = "a"
+r = "aman Kumar"
+
+print('I caught ' + str(butterflies )+ ' 🦋 butterflies!')
+print('I caught ' + str(beetles) + ' 🪲 beetles!')
+print('I caught ' + str(ladybugs )+ ' 🐞 ladybugs!')
+total = butterflies + beetles + ladybugs
+print('I caught ' + str(total) + ' total bugs!')
+print(type(ladybugs))
+print(ord(v))
+print(r[0],r[-4])
+print(r[0:5:1])
+print(ladybugs/5)# explicit type conversion done by puthon itself output 4.0
